e8756462e683cfefd9f1f1236a5c232d3d8f1b16
seanclark <sclark9999@verizon.net>
2025-09-04 20:02:22 -0400

    updated promt_sketch

--- prompt_sketch.py
+++ prompt_sketch.py
@@ -1,24 +1,24 @@
-import replicate
-import os
-import requests
-import datetime
-from config import STYLE_MAP
-from dotenv import load_dotenv
+# import replicate
+# import os
+# import requests
+# import datetime
+# from config import STYLE_MAP
+# # from dotenv import load_dotenv
 
-load_dotenv()
-REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
+# load_dotenv()
+# REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
 
-def generate_sketch(prompt, style):
+# def generate_sketch(prompt, style):
     
-    style_modifier = STYLE_MAP.get(style, "")
-    full_prompt = f"{style_modifier}, {prompt}"
+#     style_modifier = STYLE_MAP.get(style, "")
+#     full_prompt = f"{style_modifier}, {prompt}"
 
-    output = replicate.run("black-forest-labs/flux-schnell", input={"prompt": full_prompt})
-    image_url = output[0]
+#     output = replicate.run("black-forest-labs/flux-schnell", input={"prompt": full_prompt})
+#     image_url = output[0]
 
-    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    filename = f"sketch_{style}_{timestamp}.png"
+#     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+#     filename = f"sketch_{style}_{timestamp}.png"
 
-    response = requests.get(image_url)
-    with open(filename, "wb") as f:
-        f.write(response.content)
+#     response = requests.get(image_url)
+#     with open(filename, "wb") as f:
+#         f.write(response.content)
