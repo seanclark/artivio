d9744a48058d29a4c8b5aacaee609bf66ebd18d5
Sean Clark <sclark9999@verizon.net>
2025-11-08 23:40:38 -0500

    Add redirect and url_for imports in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 from config import STYLE_MAP
 import replicate
 import requests
